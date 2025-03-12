f9d61c666a5b65a545fc3db75bbef6dee77499c4
hv90 <hugomachado182@gmail.com>
2025-03-12 15:57:50 -0300

    add year range component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import DatePicker from "@/components/DatePicker";
+import YearRange from "@/components/YearRange";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <DatePicker />
+        <YearRange />
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
